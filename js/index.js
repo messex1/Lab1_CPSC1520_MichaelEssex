516a3ea0aa57c7014ea3b7f79b6b1da867e92fd8
Michael <112738703+messex1@users.noreply.github.com>
2024-01-26 00:11:48 -0700

    Tasks 1-6 complete

--- js/index.js
+++ js/index.js
@@ -1,5 +1,88 @@
-var button = document.getElementById('speedTestButton');
+// Task 1: Call to Action Button
+var button = document.querySelector('#speedTestButton');
 
 button.addEventListener('click', function() {
     console.log('Button was clicked!');
 });
+//end of task 1
+
+
+// TASK 2 dialog
+var emailDialog = document.querySelector('#emailDialog');
+
+emailDialog.addEventListener('click', function() {
+    console.log('Email Dialog was clicked!');
+});
+// end of task 2
+
+
+// TASK 3 close dialog button
+var closeButton = document.querySelector('#closeButton')
+
+closeButton.addEventListener('click', function() {
+    console.log('Close button was clicked!');
+});
+// end of TASK 3
+
+// TASK 4
+//done
+
+// TASK 5  Reference Variables
+//done
+
+// TASK 6  Add Event Listeners
+   // Call to Action Button
+   function openDialog() {
+    console.log('Open dialog');
+    }
+   button.addEventListener("click", openDialog);
+
+   // Close Dialog Button
+    function closeDialog() {
+     console.log('Close dialog');
+     }
+   closeButton.addEventListener("click", closeDialog);
+// end of TASK 6
+
+// TASK 7  Event Handler Function
+// end of TASK 7
+
+// TASK 8  Testing
+// end of TASK 8
+
+
+
+// Optional Task: Clicking Outside the modal
+
+
+
+// Task 9: Add a Submit Event to the Form.
+// end of Task 9
+
+// Task 10: Get Form Input Values
+// end of Task 10
+
+// Task 11: Format and Display the Profile Full Name
+// end of Task 11
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
